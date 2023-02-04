37a6fa338b2e46d65ffbc3917281413e9a025407
alexey <bsmegafon@yandex.ru>
2023-02-04 15:52:50 +0300

    add validation on StatController.java

--- stats/stats-server/src/main/java/ru/practicum/stats/controller/StatController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/controller/StatController.java
@@ -5,24 +5,27 @@ import dto.RequestStat;
 import dto.ResponseStat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import ru.practicum.stats.entity.StatEntity;
 import ru.practicum.stats.mapper.StatMapper;
 import ru.practicum.stats.service.StatService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@Validated
 public class StatController {
     private final StatService statService;
     private final StatMapper statMapper;
 
     @PostMapping("/hit")
-    public void save(@RequestBody RequestStat requestStat) {
+    public void save(@Valid @RequestBody RequestStat requestStat) {
         StatEntity statEntity = statMapper.toStatEntity(requestStat);
         statService.save(statEntity);
         log.info("StatService.save: " + requestStat.toString() + " /hit");
